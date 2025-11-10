6b168a62313692735881dc06bf560bd3040e6c76
Alex Carvalho <167354037+alexcarvalhosantos@users.noreply.github.com>
2025-11-10 16:47:27 -0300

    basic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,42 +1,55 @@
-const jwt = require('jsonwebtoken');
-const tokenList = {};
 const config = {
   tokenLife: 9000000,
   refreshTokenLife: 8640000,
 };
 
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const tokenList = {};
+
 exports.signinHandlerToken = async (req, res) => {
   try {
-    console.log('login')
-    const { login, senha } = req.body;
-    console.log(login, senha)
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).send('Credenciais ausentes.');
+    }
+
+    // Decodifica "Basic base64(login:senha)"
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+    const [login, senha] = credentials.split(':');
 
     if (!(login && senha)) {
       return res.status(400).send('Preencha todos os campos.');
     }
 
-    if (login == 'alex' && senha == 'frete1teste') {
-      const token = jwt.sign({ login, senha }, 'TMU2hZL4e$hqK$4Z', {
+    if (login === 'alex' && senha === 'frete1teste') {
+      const token = jwt.sign({ login }, 'TMU2hZL4e$hqK$4Z', {
         expiresIn: config.tokenLife,
       });
-      const refreshToken = jwt.sign(
-        { login, senha },
-        '4TcprVu6ZkxKqM32J#Z^3P',
-        { expiresIn: config.refreshTokenLife }
-      );
+
+      const refreshToken = jwt.sign({ login }, '4TcprVu6ZkxKqM32J#Z^3P', {
+        expiresIn: config.refreshTokenLife,
+      });
+
       const response = {
         status: 'Logado',
-        token: token,
-        refreshToken: refreshToken,
+        token,
+        refreshToken,
       };
+
       tokenList[refreshToken] = response;
       return res.status(200).json(response);
     }
+
     return res.status(400).send('Usuário ou senha inválido.');
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    return res.status(500).send('Erro interno.');
   }
 };
+
 
 exports.refreshHandler = (req, res) => {
   const { login, password, refreshToken } = req.body;
